ce7abbea304dcc15e812d9aff4f62d423da218ad
Gustavo Juneo Santos <gustavojuneo@outlook.com>
2022-04-04 00:49:45 -0300

    configurate vscode debug

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,15 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   return response.json({ message: "Hello World" });
+});
+
+app.get("/courses", (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
 });
 
 app.listen(3333, () => console.log("Server is running on port 3333!"));
